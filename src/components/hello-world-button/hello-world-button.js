021b5b909acaca8d4a1d07edb352794bd1e3685b
shirshandu <shirshandu.bhattacharjee@cerner.com>
2020-02-17 08:23:44 +0530

    integrating with jquery n custom-css

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,36 +1,19 @@
 import './hello-world-button.scss';
 
-// // import helloWorld from './hello-world-button';
-// // function helloWorld() {
-// //   console.log('Hello world');
-// // }
-
-// class helloWorldButton {
-//   render(){
-//     const button = document.createElement('button');
-//     button.innerHTML = 'Hello World';
-//     const body = document.querySelector('body');
-//     body.appendChild(button)
-//   }
-// }
-
-// export default helloWorldButton;
-
 class HelloWorldButton {
-  // buttonCssClass = 'hello-world-text';
-  // testClass = 'hello-world-text';
+    buttonCssClass = 'hello-world-button';
+
     render() {
         const button = document.createElement('button');
-        button.innerHTML = 'WebPack';
-        button.classList.add('hello-world-button');
         const body = document.querySelector('body');
+        button.innerHTML = 'Hello world';
         button.onclick = function () {
             const p = document.createElement('p');
-            p.innerHTML = 'Webpack List';
+            p.innerHTML = 'Hello world';
             p.classList.add('hello-world-text');
-            // p.classList.add(this.testClass);
             body.appendChild(p);
         }
+        button.classList.add('hello-world-button');
         body.appendChild(button);
     }
 }
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,87 +1,70 @@
-const path = require("path");
-// const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-  entry: {
-    'hello-world': './src/hello-world.js',
-    'car': './src/car.js'
-  },
-  output: {
-    // filename: 'bundle.js',
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: ""
-  },
-  mode: "production",
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-      minSize: 10000,
-      automaticNameDelimiter: '_'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg)$/,
-        use: ["file-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env"],
-            plugins: ["transform-class-properties"]
-          }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/static/'
+    },
+    mode: 'production',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 10000,
+            automaticNameDelimiter: '_'
         }
-      },
-      {
-        test: /\.hbs$/,
-        use: [
-          'handlebars-loader'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ 'stage-0' ]
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
         ]
-      }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css'
+        }),
+        new CleanWebpackPlugin('dist'),
+        new HtmlWebpackPlugin({
+            title: 'Hello world',
+            description: 'Hello world',
+            template: 'src/page-template.hbs'
+        })
     ]
-  },
-  plugins: [
-    // new TerserPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css"
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        '**/*',
-        path.join(process.cwd(), 'build/**/*')
-      ]
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'hello-world.html',
-      chunks:['hello-world','vendors~car~hello-world'],
-      title: 'Hello World',
-      template: 'src/page-template.hbs',
-      description: 'hello-world'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'car.html',
-      chunks:['car','vendors~car~hello-world'],
-      title: 'Car',
-      template: 'src/page-template.hbs',
-      description: 'car'
-    })
-  ]
 };
